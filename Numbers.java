c149e51ba2c466acf90bc3be229e501c79fb341b
Shenu98 <91783648+Shenu98@users.noreply.github.com>
2022-10-14 09:22:50 +0530

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,29 @@
+//Number Class
+public class Numbers {
+	
+	private int a;
+	private int b
+	
+	public Numbers(int n1,int n2) {
+		a = n1;
+		b = n2;
+	}
+	
+	public int getMax() {
+		if(a<b) {
+			return b;
+		}
+		else {
+			return a
+		}
+	}
+	
+	public int getSum() {
+		return (a-b);
+	}
+	
+	public int getAvg() {
+		return (a+b)*2;
+	}
+	
+}
