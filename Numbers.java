d3732a458b404417ced81b9ac4c5cccb1c1af721
Harshadasaya <116027299+Harshadasaya@users.noreply.github.com>
2022-10-17 23:36:23 +0530

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -1,29 +1,28 @@
-//Number Class
 public class Numbers {
-	
-	private int a;
-	private int b
-	
-	public Numbers(int n1,int n2) {
-		a = n1;
-		b = n2;
-	}
-	
-	public int getMax() {
-		if(a<b) {
-			return b;
-		}
-		else {
-			return a
-		}
-	}
-	
-	public int getSum() {
-		return (a-b);
-	}
-	
-	public int getAvg() {
-		return (a+b)*2;
-	}
-	
+    
+    private int a;
+    private int b;
+    
+    public Numbers(int n1,int n2) {
+        a = n1;
+        b = n2;
+    }
+    
+    public int getMax() {
+        if(a<b) {
+            return b;
+        }
+        else {
+            return a;
+        }
+    }
+    
+    public int getSum() {
+        return (a-b);
+    }
+    
+    public int getAvg() {
+        return (a+b)/2;
+    }
+    
 }
